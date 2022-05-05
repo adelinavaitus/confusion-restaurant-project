0eee54b65412e3ee6a905431353949016620a369
Adelina Vaitus <adelinavaitus@gmail.com>
2022-05-05 13:17:01 +0300

    fetch post comments

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -31,8 +31,9 @@ class CommentForm extends Component {
 
 
     handleSubmit(values) {
+        
         this.toggleModal();
-        this.props.addComment(this.props.dishId, values.rating, values.author, values.comment);
+        this.props.postComment(this.props.dishId, values.rating, values.author, values.comment);
         
 
     }
@@ -119,7 +120,7 @@ function RenderDish({ dish }) {
     }
 }
 
-function RenderComments({ comments , addComment, dishId}) {
+function RenderComments({ comments , postComment, dishId}) {
     if (comments != null) {
         return (
             <div>
@@ -134,7 +135,7 @@ function RenderComments({ comments , addComment, dishId}) {
                         );
                     })
                 }
-                <CommentForm  dishId={dishId} addComment={addComment}/>
+                <CommentForm  dishId={dishId} postComment={postComment}/>
             </div>
 
         );
@@ -184,7 +185,7 @@ const DishDetail = (props) => {
                 </div>
                 <div className='col-12 col-md-5 m-1'>
                     <RenderComments comments={props.comments}
-                    addComment = {props.addComment}
+                    postComment = {props.postComment}
                     dishId={props.dish.id} />
 
                 </div>
